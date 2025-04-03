349fd3b87dd246dc603d66c1a3602a273d63f9c1
KateM <ekaterinamarantidi@gmail.com>
2025-04-03 00:35:23 -0700

    middle_school_student.py done, nominal and edge tests done

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,17 @@
 from .student import Student
 
-# add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation = False):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+
+    def transportation_message(self):
+        gets_message = "receives" if self.gets_transportation else "does not receive"
+
+        return f"{self.name} {gets_message} school transportation"
+    
+    def summary(self):
+        student_summary = super().summary()
+        transportation_status = self.transportation_message()
+        
+        return "\n".join([student_summary, transportation_status])
--- tests/test_middle_school_student.py
+++ tests/test_middle_school_student.py
@@ -1,25 +1,76 @@
-# from school_schedule.middle_school_student import MiddleSchoolStudent
+from school_schedule.middle_school_student import MiddleSchoolStudent
 
-# def test_new_valid_middle_school_student_gets_transportation():
-#     # Arrange
-#     name = "Ellis"
-#     grade = "junior"
-#     classes = ["Painting"]
+def test_new_valid_middle_school_student_gets_transportation():
+    # Arrange
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting"]
 
-#     # Act
-#     ellis = MiddleSchoolStudent(name, grade, classes, gets_transportation=True)
+    # Act
+    ellis = MiddleSchoolStudent(name, grade, classes, gets_transportation=True)
 
-#     assert ellis.name == name
-#     assert ellis.grade == grade
-#     assert ellis.classes == classes
-#     assert len(ellis.classes) == 1
-#     assert ellis.gets_transportation
+    assert ellis.name == name
+    assert ellis.grade == grade
+    assert ellis.classes == classes
+    assert len(ellis.classes) == 1
+    assert ellis.gets_transportation
 
 def test_new_valid_middle_school_student_with_defaults():
-    pass
+    # Arrange
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting"]
+
+    # Act
+    ellis = MiddleSchoolStudent(name, grade, classes, gets_transportation=False)
+
+    assert ellis.name == name
+    assert ellis.grade == grade
+    assert ellis.classes == classes
+    assert len(ellis.classes) == 1
+    assert not ellis.gets_transportation
 
 def test_middle_school_student_summary_with_transportation():
-    pass
+    # Arrange
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting"]
+    ellis = MiddleSchoolStudent(name, grade, classes, gets_transportation=True)
+    summary = ellis.summary()
+
+    assert summary == "Ellis is a junior enrolled in 1 classes: Painting\nEllis receives school transportation"
 
 def test_middle_school_student_summary_without_transportation():
-    pass
+    # Arrange
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting"]
+    ellis = MiddleSchoolStudent(name, grade, classes, gets_transportation=False)
+    summary = ellis.summary()
+
+    assert summary == "Ellis is a junior enrolled in 1 classes: Painting\nEllis does not receive school transportation"
+
+def test_new_valid_middle_school_student_nonstr_attribute_classes():
+    # Arrange
+    name = "Ellis"
+    grade = 7
+    classes = "yes"
+
+    # Act
+    ellis = MiddleSchoolStudent(name, grade, classes, gets_transportation=False)
+
+    assert ellis.classes is list
+    assert isinstance(ellis.classes, list)
+
+
+
+def test_middle_school_student_summary_nonbulean_transportation():
+    # Arrange
+    name = "Ellis"
+    grade = "junior"
+    classes = ["Painting"]
+
+    # Act
+    ellis = MiddleSchoolStudent(name, grade, classes, gets_transportation="yes")
+
+    assert isinstance(ellis.gets_transportation, bool)
